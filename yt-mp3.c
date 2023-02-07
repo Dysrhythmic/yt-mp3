b4bfad630574918298544e242a17acfd1fac9c7b
Dysrhythmic <83474789+Dysrhythmic@users.noreply.github.com>
2023-02-06 22:55:06 -0600

    Make path option more general

--- yt-mp3.c
+++ yt-mp3.c
@@ -7,7 +7,7 @@ int main() {
   printf("Enter the URL: ");
   scanf("%s", url);
 
-  char opts[] = "yt-dlp -P \"/mnt/d/Audio/Music\" -x --audio-format mp3 -o \"%(title)s.%(ext)s\" ";
+  char opts[] = "yt-dlp -P \"./music\" -x --audio-format mp3 -o \"%(title)s.%(ext)s\" ";
   char *command = strcat(opts, url);
 
   system(command);
