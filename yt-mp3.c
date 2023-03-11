de9e8f28fea1e6bf70acc64e146182d5b578dee8
Dysrhythmic <83474789+Dysrhythmic@users.noreply.github.com>
2023-03-11 14:09:31 -0600

    split path from opts

--- yt-mp3.c
+++ yt-mp3.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  char opts[] = "yt-dlp -P \"/mnt/d/Audio/Music\" -x --audio-format mp3 -o \"%(title)s.%(ext)s\" ";
-  char *command = (char*)malloc(strlen(opts) + strlen(argv[1]) + 1);
+  char path[] = "/mnt/d/Audio/Music ";
+  char opts[] = "yt-dlp -x --audio-format mp3 -o \"%(title)s.%(ext)s\" -P ";
+  char *command = (char*)malloc(strlen(opts) + strlen(path) + strlen(argv[1]) + 1);
   strcpy(command, opts);
+  strcat(command, path);
   strcat(command, argv[1]);
   system(command);
   free(command);
