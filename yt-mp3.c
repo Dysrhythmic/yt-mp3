a114b394bbd8b3f3b86443f5475bdc364dba6e8f
Dysrhythmic <83474789+Dysrhythmic@users.noreply.github.com>
2023-03-11 13:17:31 -0600

    get URL as CLI arg instead of asking for input

--- yt-mp3.c
+++ yt-mp3.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-int main() {
-  char url[100];
-  printf("Enter the URL: ");
-  scanf("%s", url);
-
-  char opts[] = "yt-dlp -P \"./music\" -x --audio-format mp3 -o \"%(title)s.%(ext)s\" ";
-  char *command = strcat(opts, url);
-
+int main(int argc, char *argv[]) {
+  char opts[] = "yt-dlp -P \"/mnt/d/Audio/Music\" -x --audio-format mp3 -o \"%(title)s.%(ext)s\" ";
+  char *command = (char*)malloc(strlen(opts) + strlen(argv[1]) + 1);
+  strcpy(command, opts);
+  strcat(command, argv[1]);
   system(command);
+  free(command);
 }
